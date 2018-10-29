a1eaa55e3ab07caf4f4e0c419cea6a449acda913
Daniel Hollands <daniel@limeblast.co.uk>
2018-10-29 18:14:48 +0000

    Switching to an Adafruit IP based approach. This is some initial boilerplate code to send and recieve data

--- include/config_sample.h
+++ include/config_sample.h
@@ -1,9 +1,21 @@
+/************************ Adafruit IO Config *******************************/
 
-/* Copy this file, rename it `config.h`, then set the relevant data below */
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME "your_username"
+#define IO_KEY "your_key"
 
-/************************* MQTT Setup *********************************/
+/******************************* WIFI **************************************/
 
-#define MQTT_SERVER      "io.adafruit.com"
-#define MQTT_SERVERPORT  1883
-#define MQTT_USERNAME    ""
-#define MQTT_KEY         ""
+// the AdafruitIO_WiFi client will work with the following boards:
+//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
+//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
+//   - Feather HUZZAH ESP32 -> https://www.adafruit.com/product/3405
+//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+//   - Feather WICED -> https://www.adafruit.com/products/3056
+
+#define WIFI_SSID "your_ssid"
+#define WIFI_PASS "your_pass"
+
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
--- src/main.ino
+++ src/main.ino
@@ -1,161 +1,105 @@
-/***************************************************
-  Adafruit MQTT Library ESP8266 Example
+// Adafruit IO Publish & Subscribe Example
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
 
-  Must use ESP8266 Arduino from:
-    https://github.com/esp8266/Arduino
+/************************** Configuration ***********************************/
 
-  Works great with Adafruit's Huzzah ESP board:
-  ----> https://www.adafruit.com/product/2471
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Tony DiCola for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
-#include <NTPClient.h>        //Used to get a timestamp
-#include <ESP8266WiFi.h>      //ESP8266 Core WiFi Library
-#include <DNSServer.h>        //Local DNS Server used for redirecting all requests to the configuration portal
-#include <ESP8266WebServer.h> //Local WebServer used to serve the configuration portal
-#include <WiFiManager.h>      //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
-
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
 #include "config.h"
 
-/************ Global State (you don't need to change this!) ******************/
+/************************ Example Starts Here *******************************/
 
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
-WiFiClient client;
+// this int will hold the current count for our sketch
+int count = 0;
 
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
+// Track time of last published messages and limit feed->save events to once
+// every IO_LOOP_DELAY milliseconds.
+//
+// Because this sketch is publishing AND subscribing, we can't use a long
+// delay() function call in the main loop since that would prevent io.run()
+// from being called often enough to receive all incoming messages.
+//
+// Instead, we can use the millis() function to get the current time in
+// milliseconds and avoid publishing until IO_LOOP_DELAY milliseconds have
+// passed.
+#define IO_LOOP_DELAY 5000
+unsigned long lastUpdate = 0;
 
-/****************************** Feeds ***************************************/
-
-Adafruit_MQTT_Subscribe input = Adafruit_MQTT_Subscribe(&mqtt, MQTT_USERNAME "/feeds/wildcat.incident");
-Adafruit_MQTT_Publish output = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "/feeds/wildcat.incident");
-
-/****************************** IO *****************************************/
-
-int timestamp = 0;
-
-#define BUTTON_PIN 2
-
-/****************************** Timestamp ***********************************/
-
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
-
-/*************************** Sketch Code ************************************/
-
-void inputcallback(double x)
-{
-    Serial.print("Input value is: ");
-    Serial.println((int)x);
-    timestamp = (int)x;
-}
+// set up the 'counter' feed
+AdafruitIO_Feed *counter = io.feed("counter");
 
 void setup()
 {
+    // start the serial connection
     Serial.begin(9600);
-    delay(10);
 
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    // wait for serial monitor to open
+    while (!Serial);
 
-    // updateStatus();
+    Serial.print("Connecting to Adafruit IO");
 
-    WiFiManager wifiManager;
-    wifiManager.setTimeout(180);
+    // connect to io.adafruit.com
+    io.connect();
 
-    if (!wifiManager.autoConnect("Wildcat"))
+    // set up a message handler for the count feed.
+    // the handleMessage function (defined below)
+    // will be called whenever a message is
+    // received from adafruit io.
+    counter->onMessage(handleMessage);
+
+    // wait for a connection
+    while (io.status() < AIO_CONNECTED)
     {
-        Serial.println("failed to connect and hit timeout");
-        delay(3000);
-        ESP.reset();
-        delay(5000);
+        Serial.print(".");
+        delay(500);
     }
 
-    Serial.println("connected...yeey :)");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    // we are connected
+    Serial.println();
+    Serial.println(io.statusText());
 
-    input.setCallback(inputcallback);
-
-    mqtt.subscribe(&input);
-
-    timeClient.begin();
+    // get last value
+    counter->get();
 }
-
-uint32_t x = 0;
 
 void loop()
 {
-    // Ensure the connection to the MQTT server is alive (this will make the first
-    // connection and automatically reconnect when disconnected).  See the MQTT_connect
-    // function definition further below.
-    MQTT_connect();
+    // io.run(); is required for all sketches.
+    // it should always be present at the top of your loop
+    // function. it keeps the client connected to
+    // io.adafruit.com, and processes any incoming data.
+    io.run();
 
-    timeClient.update();
-    Serial.println(timeClient.getFormattedTime());
-    Serial.println(timeClient.getEpochTime());
-
-    // If the button is pressed
-    if (digitalRead(BUTTON_PIN) == LOW)
+    if (millis() > (lastUpdate + IO_LOOP_DELAY))
     {
-        Serial.println("Button pressed");
-        // Wait for the button to be released
-        while (digitalRead(BUTTON_PIN) == LOW)
-        {
-            delay(100);
-        }
-        Serial.println("Button released");
-        // Publish a button pushed message to a topic
-        output.publish(123);
-    }
+        // save count to the 'counter' feed on Adafruit IO
+        Serial.print("sending -> ");
+        Serial.println(count);
+        counter->save(count);
 
-    // this is our 'wait for incoming subscription packets and callback em' busy subloop
-    // try to spend your time here:
-    mqtt.processPackets(10000);
+        // increment the count by 1
+        count++;
 
-    // updateStatus();
-
-    // ping the server to keep the mqtt connection alive
-    // NOT required if you are publishing once every KEEPALIVE seconds
-    if (!mqtt.ping())
-    {
-        mqtt.disconnect();
+        // after publishing, store the current time
+        lastUpdate = millis();
     }
 }
 
-// Function to connect and reconnect as necessary to the MQTT server.
-// Should be called in the loop function and it will take care if connecting.
-void MQTT_connect()
+// this function is called whenever a 'counter' message
+// is received from Adafruit IO. it was attached to
+// the counter feed in the setup() function above.
+void handleMessage(AdafruitIO_Data *data)
 {
-    int8_t ret;
-
-    // Stop if already connected.
-    if (mqtt.connected())
-    {
-        return;
-    }
-
-    Serial.print("Connecting to MQTT... ");
-
-    uint8_t retries = 3;
-    while ((ret = mqtt.connect()) != 0)
-    { // connect will return 0 for connected
-        Serial.println(mqtt.connectErrorString(ret));
-        Serial.println("Retrying MQTT connection in 10 seconds...");
-        mqtt.disconnect();
-        delay(10000); // wait 10 seconds
-        retries--;
-        if (retries == 0)
-        {
-            // basically die and wait for WDT to reset me
-            while (1);
-        }
-    }
-    Serial.println("MQTT Connected!");
+    Serial.print("received <- ");
+    Serial.println(data->value());
 }
